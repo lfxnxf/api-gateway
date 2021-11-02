215ddf3d720c1e5dfbdfbfff14f2195e8e3d7754
“xuefeng” <xuefeng@soyoung.com>
2021-11-02 16:52:42 +0800

    第一次提交

---
+++ app/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/lfxnxf/frame/BackendPlatform/golang/logging"
+	"github.com/lfxnxf/frame/logic/inits"
+	"github.com/school/api-gateway/conf"
+	"github.com/school/api-gateway/server/http"
+	"github.com/school/api-gateway/service"
+)
+
+func init() {
+	configS := flag.String("config", "config/config.toml", "Configuration file")
+	appS := flag.String("app", "", "App dir")
+	flag.Parse()
+
+	inits.Init(
+		inits.ConfigPath(*configS),
+	)
+
+	if *appS != "" {
+		inits.InitNamespace(*appS)
+	}
+
+}
+
+func main() {
+
+	defer inits.Shutdown()
+
+	// init local config
+	cfg, err := conf.Init()
+	if err != nil {
+		logging.Fatalf("service config init error %s", err)
+	}
+
+	// create a service instance
+	srv := service.New(cfg)
+
+	// init and start http server
+	http.Init(srv, cfg)
+
+	defer http.Shutdown()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	for {
+		s := <-sigChan
+		log.Printf("get a signal %s\n", s.String())
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			log.Println("api-gateway server exit now...")
+			return
+		case syscall.SIGHUP:
+		default:
+		}
+	}
+}
