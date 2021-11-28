70d13d5f5af483102329175f9a7b6b4a79c3e88e
“xuefeng” <xuefeng@soyoung.com>
2021-11-28 14:01:18 +0800

    ###

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+docker build --rm -t school-api-gateway .
 docker stop api-gateway-1
 docker rm api-gateway-1
 docker rmi school-api-gateway-1
-docker build --rm -t school-api-gateway .
 docker run -d -p 10000:10000 -v /work/go/logs/api-gateway:/logs  --name api-gateway-1 school-api-gateway
 
+docker build --rm -t school-api-gateway .
 docker stop api-gateway-2
 docker rm api-gateway-2
 docker rmi school-api-gateway-2
-docker build --rm -t school-api-gateway .
 docker run -d -p 10001:10000 -v /work/go/logs/api-gateway:/logs  --name api-gateway-2 school-api-gateway
