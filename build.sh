4e310528db503f1d364edaaf02f639867bfe78f4
“xuefeng” <xuefeng@soyoung.com>
2021-11-28 14:02:07 +0800

    ###

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ docker rm api-gateway-1
 docker rmi school-api-gateway-1
 docker run -d -p 10000:10000 -v /work/go/logs/api-gateway:/logs  --name api-gateway-1 school-api-gateway
 
-docker build --rm -t school-api-gateway .
 docker stop api-gateway-2
 docker rm api-gateway-2
 docker rmi school-api-gateway-2
